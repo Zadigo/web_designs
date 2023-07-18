3b1bd97ce832348b935717ca38c17735b160f888
Zadigo <pendenquejohn@gmail.com>
2023-07-18 16:00:41 +0200

    Update local storage functions

--- vue_localstorage.js
+++ vue_localstorage.js
@@ -23,10 +23,16 @@ class VueLocalStorage {
     _getValueForOperation(key) {
         // NEW: Return the numeric value stored under a key
         // to run a given operation
-        var storedData = this.data
-        let result = storedData[key]
+        // var storedData = this.data
+        // let result = storedData[key]
+        const result = this.retrieve(key)
+
         if (result === undefined) {
             return 0
+        }
+
+        if (typeof result !== 'number') {
+            throw new Error('Value for increment or decrement operation should be a number')
         }
         return result
     }
@@ -51,56 +57,164 @@ class VueLocalStorage {
         storedData[key] = value
         this._save(storedData)
     }
+
+    exists (key) {
+        // NEW
+        const storedData = this.data
+        return Object.keys(storedData).includes(key)
+    }
     
     getDelete(key) {
         // NEW
         let returnValue
         var storedData = this.data
+
         returnValue = storedData[key]
         delete storedData[key]
+
         this._save(storedData)
         return returnValue
     }
 
     increment(key) {
         // NEW
-        var storedData = this.data
+        // var storedData = this.data
+        // let result = this._getValueForOperation(key)
+        // storedData[key] = result += 1
+        // this._save(storedData)
         let result = this._getValueForOperation(key)
-        storedData[key] = result += 1
-        this._save(storedData)
+        result = result += k
+        this.create(key, result)
     }
 
     decrement(key) {
         // NEW
-        var storedData = this.data
+        // var storedData = this.data
+        // let result = this._getValueForOperation(key)
+        // storedData[key] = result += 1
+        // this._save(storedData)
         let result = this._getValueForOperation(key)
-        storedData[key] = result += 1
-        this._save(storedData)
+        result = result -= k
+        this.create(key, result)
     }
 
     incrementBy(key, k=1) {
         // NEW
-        var storedData = this.data
+        // var storedData = this.data
+        // let result = this._getValueForOperation(key)
+        // storedData[key] = result += k
+        // this._save(storedData)
         let result = this._getValueForOperation(key)
-        storedData[key] = result += k
-        this._save(storedData)
+        result = result += k
+        this.create(key, result)
     }
 
     decrementBy(key, k=1) {
         // NEW
-        var storedData = this.data
+        // var storedData = this.data
+        // let result = this._getValueForOperation(key)
+        // storedData[key] = result -= k
+        // this._save(storedData)
         let result = this._getValueForOperation(key)
-        storedData[key] = result -= k
-        this._save(storedData)
+        result = result -= k
+        this.create(key, result)
+    }
+
+    getOrCreate (key, value) {
+        // NEW
+        // var storedData = this.data
+        // let result = storedData[key]
+        // if (!result) {
+        //     // storedData[key] = value
+        //     // this._save(storedData)
+        //     this.create(key, value)
+        // } else {
+        //     return result
+        // }
+        const result = this.exists(key)
+        let returnValue = null
+        let returnArray
+        if (result) {
+            returnValue = this.retrieve(key)
+            returnArray = [false, returnValue]
+        } else {
+            this.create(key, value)
+            returnValue = value
+            returnArray = [true, returnValue]
+        }
+        return returnArray
     }
 
     listPush(key, value) {
         // NEW
-        var storedData = this.data
+        // var storedData = this.data
+        // let result = this._getList(key)
+        // result.push(value)
+        // storedData[key] = result
+        // this._save(storedData)
         let result = this._getList(key)
         result.push(value)
-        storedData[key] = result
-        this._save(storedData)
+        this.create(key, result)
+    }
+
+    // listFilter(key, value) {
+    //     // NEW
+    //     const result = this._getList(key)
+    //     return result.filter((item) => {
+    //         let truthArray = [
+    //             item.includes(value),
+    //             item === value
+    //         ]
+    //         return truthArray.some(x => x === true)
+    //     })
+    // }
+
+    listMerge (key, values) {
+        // NEW
+        // let newList
+        // var storedData = this.data
+        // const result = this._getList(key)
+
+        // if (!Array.isArray(values)) {
+        //     throw new Error('Is not an array')
+        // }
+
+        // newList = [...result, ...values]
+        // storedData[key] = newList
+        // this._save(storedData)
+        let newList
+        const result = this._getList(key)
+
+        if (!Array.isArray(values)) {
+            throw new Error('Is not an array')
+        }
+
+        newList = [...result, ...values]
+        this.create(key, newList)
+    }
+
+    listCount (key) {
+        // NEW
+        const result = this._getList(key)
+        return result.length
+    }
+
+    toggle (key) {
+        // NEW
+        // var storedData = this.data
+        // let result = storedData[key]
+        // if (typeof result === 'boolean') {
+        //     storedData[key] = !storedData[key]
+        // } else {
+        //     storedData[key] = true
+        // }
+        // this._save(storedData)
+        var result = this.retrieve(key)
+        if (typeof result === 'boolean') {
+            this.create(key, !result)
+        } else {
+            this.create(key, true)
+        }
     }
 
     // createExpiry(key, value) {
@@ -120,17 +234,13 @@ class VueLocalStorage {
     // }
 
     remove(key) {
-        var result = this.data
-        delete result[key]
-        this._save(result)
+        var storedData = this.data
+        delete storedData[key]
+        this._save(storedData)
     }
 
     save(key, value) {
         this.storage.setItem(key, value)
-    }
-
-    getValue(key) {
-        return this.storage.getItem(key)
     }
 
     install(app) {
