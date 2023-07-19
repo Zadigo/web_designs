22277c5525bd18fa0e70d26304b2aa570fffa949
Zadigo <pendenquejohn@gmail.com>
2023-07-19 14:23:49 +0200

    Implement workflows

--- vue_bootstrap.js
+++ vue_bootstrap.js
@@ -221,6 +221,108 @@ const BasePagination = {
     }
 }
 
+const NavPills = {
+    name: 'NavPills',
+    props: {
+        items: {
+            type: Array,
+            default() {
+                return []
+            }
+        },
+        position: {
+            type: String
+        },
+        horizontal: {
+            type: Boolean,
+            default: true
+        },
+        isTabs: {
+            type: Boolean
+        },
+        isPills: {
+            type: Boolean
+        },
+        justified: {
+            type: Boolean
+        },
+        fill: {
+            type: Boolean
+        },
+        pillClasses: {
+            type: Object,
+            default() {
+                return {}
+            }
+        },
+        inCard: {
+            type: Boolean
+        }
+    },
+    emits: {
+        change() {
+            return true
+        },
+        'click:nav-pill'() {
+            return true
+        }
+    },
+    template: `
+    <div class="wrapper">
+        <ul :class="navpillClasses">
+        <li class="nav-item">
+            <a v-for="(item, i) in items" :key="i" :aria-current="i == selectedTab" :class="{ active: i === selectedTab }" class="nav-link" href @click.prevent="selectItem(i, item)">
+                [[ item.name ]]
+            </a>
+        </li>
+        </ul>
+        
+        <transition name="slide" mode="out-in">
+        <slot :selected-tab="selectedTab"></slot>
+        </transition>
+    </div>
+    `,
+    data() {
+        return {
+            selectedTab: 0
+        }
+    },
+    computed: {
+        currentTab() {
+            return this.items[this.selectedTab]
+        },
+        navpillClasses() {
+            return [
+                'nav',
+                this.pillClasses,
+                {
+                    'justify-content-center': this.position === 'center',
+                    'justify-content-end': this.position === 'end',
+                    'flex-column': !this.horizontal,
+                    'nav-tabs': this.isTabs,
+                    'nav-pills': this.isPills,
+                    'nav-fill': this.fill,
+                    'nav-justified': this.justified,
+                    'card-header-tabs': this.isPills && this.inCard,
+                    'card-header-pills': this.isTabs && this.inCard
+                }
+            ]
+        }
+    },
+    watch: {
+        selected(current, previous) {
+            this.$emit('change', { current: current, previous: previous })
+        }
+    },
+    methods: {
+        selectItem(index, item) {
+            this.selectedTab = index
+            this.$emit('click:nav-pill', [index, item])
+        }
+    }
+}
+
 app.component('BaseCheckbox', BaseCheckBox)
 app.component('BaseInput', BaseInput)
 app.component('BasePagination', BasePagination)
+app.component('NavPills', NavPills)
